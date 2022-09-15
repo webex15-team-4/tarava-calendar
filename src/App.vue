8a4ec2b4ccaf7bd3e5786a3a93cfdbe44dc4d997
Sae2611 <fuwafuwa355@gmai.com>
2022-09-15 22:20:00 +0900

    色をボタン機能にしたい

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,4 @@
 <template>
-
   <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
@@ -7,24 +6,24 @@
   <router-view /> -->
   <CommentApp />
   <ScheduleApp />
-
+  <ColorApp />
   <!-- <nav><router-link to="/">Home</router-link> |</nav> -->
 
   <Calendar />
-
 </template>
 
 <script>
 import CommentApp from "./components/CommentApp.vue"
 import ScheduleApp from "@/components/ScheduleApp.vue"
 import Calendar from "@/components/CalendarVue.vue"
+import ColorApp from "@/components/ColorApp.vue"
 //import Login from "@/components/LoginView.vue"
 export default {
   components: {
     CommentApp,
     ScheduleApp,
     Calendar,
-    //Login,
+    ColorApp,
   },
 }
 </script>
